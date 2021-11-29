a332cbabe323c3756413127051168416eb3d58f1
Neto <fpottinger.marques@gmail.com>
2021-11-29 14:44:23 +0000

    added overall number of students to the directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#print list with all the students
 puts "The students of Villains Academy"
 puts "--------------------------------"
 puts "Doctor Hannibal Lecter"
@@ -11,3 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+#print overall number of students
+print "Overall, we have "
+print 11
+puts " great students"
